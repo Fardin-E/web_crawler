166a1244ba8b6788b9f92cfba3844c893a17a6ea
Fardin Rahman <71291566+Fardin-E@users.noreply.github.com>
2025-10-23 15:13:42 -0400

    Update queue.go

--- crawler/queue.go
+++ crawler/queue.go
@@ -3,6 +3,7 @@ package crawler
 import (
 	"math/rand"
 	"net/url"
+	"sync"
 
 	"github.com/Fardin-E/web_crawler.git/frontier"
 
@@ -20,18 +21,35 @@ func distributeUrls(frontier *frontier.Frontier, distributedInputs []chan *url.U
 		}
 		distributedInputs[index] <- url
 	}
+
+	// Close all worker input channels when frontier is exhausted
+	log.Debug("Frontier exhausted, closing worker input channels")
+	for _, ch := range distributedInputs {
+		close(ch)
+	}
 }
 
 func mergeResults(workerResults []chan CrawlResult, out chan CrawlResult) {
+	var wg sync.WaitGroup
+
 	collect := func(in chan CrawlResult) {
+		defer wg.Done()
 		for result := range in {
 			out <- result
 		}
-		log.Println("Worker finished")
+		log.Debug("Worker finished sending results")
 	}
 
 	for i, result := range workerResults {
 		log.Printf("Start collecting results from worker %d", i)
+		wg.Add(1)
 		go collect(result)
 	}
+
+	// Wait for all workers to finish, then close output channel
+	go func() {
+		wg.Wait()
+		log.Debug("All workers finished, closing merged results channel")
+		close(out)
+	}()
 }
